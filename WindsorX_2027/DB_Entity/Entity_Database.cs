ec4ff97ca3287fbde5a4721a5acd298dc988eb95
KnudNielsen999 <jervelund.nielsen@hotmail.com>
2024-10-31 11:37:01 +0100

    start på indkøbsmodel har fået lavet modeller + entity delen med modelbuilder

--- WindsorX_2027/DB_Entity/Entity_Database.cs
+++ WindsorX_2027/DB_Entity/Entity_Database.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WindsorX_2027.IndkoebsModel;
 using WindsorX_2027.LagerModel;
 
 namespace WindsorX_2027.DB_Entity
@@ -9,5 +10,20 @@ namespace WindsorX_2027.DB_Entity
 
         // DbSet-egenskaber for dine entiteter
         public DbSet<Lagermodel> LagerData { get; set; }
+        public DbSet<IndkobModel> IndkobsOrdre { get; set; }
+        public DbSet<OrdreModel> OrdreLinjer { get; set; }
+        public DbSet<LeverandorReg> Leverandorer { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IndkobModel>()
+                .HasMany(i => i.ordreLinjer)   // Brug ordreLinjer, som er en liste af OrdreModel
+                .WithOne()                      // Definer relateret entitet
+                .HasForeignKey(o => o.IndkobModelId);  // Tilføj fremmednøgle (kræver IndkobModelId i OrdreModel)
+        }
+
+
     }
+
+
 }
---
+++ WindsorX_2027/IndkoebsModel/IndkobModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using WindsorX_2027.LagerModel;
+
+namespace WindsorX_2027.IndkoebsModel
+{
+    public class IndkobModel
+    {
+        public int Id { get; set; }
+        public string? ordreNummer { get; set; }
+        public string? kundeNummer { get; set; }
+        public DateTime ordreDato { get; set; }
+        public string? ordreDetaljer { get; set; }
+        public string? referenceDetaljer { get; set; }
+        public string? leverandorNummer { get; set; }
+
+        public List<OrdreModel> ordreLinjer { get; set; } = new List<OrdreModel>();
+
+    }
+    public class OrdreModel
+    {
+        public int Id { get; set; }
+       
+        public string? leverandørNummer { get; set; }
+        public Lagermodel? vareNummer { get; set; }
+        public Lagermodel? vareTekst { get; set; }
+        public double? ordreAntal { get; set; }
+        public Lagermodel?  enheder { get; set; }
+        public Lagermodel? kostPris { get; set; }
+        public int IndkobModelId { get; set; }       // Fremmednøgle
+        public IndkobModel Indkob { get; set; }      // Navigationsejendom
+
+    }
+    public class LeverandorReg
+    {
+        public int Id { get; set; }
+        public string? leverandorNummer { get; set; }
+        public string? Firmanavn { get; set; }
+        public string? adresse { get; set; }
+
+        public string? byNavn { get; set; }
+        public string? postNummer { get; set; }
+        public string? telefonNummer1 { get; set; }
+        public string? telefonNummer2 { get; set; }
+        public string? kontaktPerson { get; set; }
+        public string? kontaktOplysninger { get; set; }
+        public string? email1 { get; set; }
+        public string? email2 { get; set;}
+
+    }
+}
